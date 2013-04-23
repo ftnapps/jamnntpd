ed1252a0aa10908f92ae8b5f5c48cd50f1d96bfd
Robert James Clay <jame@rocasa.us>
2013-04-23 16:10:42 -0400

    Add remote_fs to Required-* entries in debian/init.d.

--- debian/init.d
+++ debian/init.d
@@ -1,8 +1,8 @@
 #!/bin/sh
 ### BEGIN INIT INFO
 # Provides:          jamnntpd
-# Required-Start:    $network $local_fs
-# Required-Stop:
+# Required-Start:    $network $remote_fs
+# Required-Stop:     $remote_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: NNTP Server allowing newsreaders to access a JAM messagebase
